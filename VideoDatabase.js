c42181a1b81caf1142c71a403e36c866155dd75a
We-Are-1 <caring.01@protonmail.com>
2024-11-06 16:15:40 +0700

    Update VideoDatabase.js add HLS

--- VideoDatabase.js
+++ VideoDatabase.js
@@ -5,6 +5,7 @@
         {
             "name": "1. giam stress CEO",
             "url": "https://8d86a98a0a9426a560f8d992.blob.core.windows.net/video-output/XGkXGz5YuAaQTqURjwdRKD/cmaf/manifest.mpd",
+            "hlsUrl": "https://8d86a98a0a9426a560f8d992.blob.core.windows.net/video-output/XGkXGz5YuAaQTqURjwdRKD/cmaf/manifest.m3u8",
             "keys": [
                 {
                     "keyId": "d7a83c00-6d4b-4054-9223-6e49ae4f63d7"
@@ -14,6 +15,7 @@
         {
             "name": "2. benh tat khong gian khac",
             "url": "https://8d86a98a0a9426a560f8d992.blob.core.windows.net/video-output/QYT7UR49LRjhLgY6zDorPL/cmaf/manifest.mpd",
+            "hlsUrl": "https://8d86a98a0a9426a560f8d992.blob.core.windows.net/video-output/QYT7UR49LRjhLgY6zDorPL/cmaf/manifest.m3u8",
             "keys": [
                 {
                     "keyId": "30a83508-7548-44fd-91ae-2c84919b8cd6"

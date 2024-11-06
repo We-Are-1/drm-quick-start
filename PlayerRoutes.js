71ae7f9b38872ec430be9a6aaf8e63801d498e14
We-Are-1 <caring.01@protonmail.com>
2024-11-06 19:34:46 +0700

    Update PlayerRoutes.js back & change cer to Fairplay Datasets

--- PlayerRoutes.js
+++ PlayerRoutes.js
@@ -101,11 +101,7 @@
                                     if (isSafari) {
                                         try {
                                             console.log('Fetching FairPlay certificate...');
-                                            const certResponse = await fetch('https://8d86a98a0a9426a560f8d992.blob.core.windows.net/web/fairplay.cer');
-                                                headers: {
-                                                    'X-AxDRM-Message': token  // Add your authentication token if required
-                                                }
-                                            });                                            
+                                            const certResponse = await fetch('https://drm-management.axprod.net/api/FairPlayDataSets');                                           
                                             if (!certResponse.ok) throw new Error('Failed to fetch certificate');
                                             fairplayCertificate = new Uint8Array(await certResponse.arrayBuffer());
                                             console.log('FairPlay certificate loaded successfully', fairplayCertificate);
